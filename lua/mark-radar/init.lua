f96a2e06996900a2d700675017f8ae91bca8ee6a
kghugo <hugosum.dev@protonmail.com>
2021-06-22 06:58:15 +0800

    Add mapping for '

--- lua/mark-radar/init.lua
+++ lua/mark-radar/init.lua
@@ -4,16 +4,22 @@ local motion = require("mark-radar.motion")
 local ns = vim.api.nvim_create_namespace("mark-radar")
 
 local function setup(user_opts)
-    opts = vim.tbl_extend('force', opts, user_opts or {})
-    vim.api.nvim_command("highlight default RadarMark guifg=#ff007c gui=bold ctermfg=198 cterm=bold")
-    vim.api.nvim_command("highlight default RadarBackground guifg=#666666 ctermfg=242")
+	opts = vim.tbl_extend("force", opts, user_opts or {})
+	vim.api.nvim_command("highlight default RadarMark guifg=#ff007c gui=bold ctermfg=198 cterm=bold")
+	vim.api.nvim_command("highlight default RadarBackground guifg=#666666 ctermfg=242")
 	local modes = { "n", "v" }
 	if opts.set_default_mappings then
 		for _, mode in ipairs(modes) do
 			vim.api.nvim_set_keymap(
 				mode,
 				"`",
-				"<cmd>lua require('mark-radar').scan()<cr>",
+				"<cmd>lua require('mark-radar').scan(true)<cr>",
+				{ silent = true, noremap = true }
+			)
+			vim.api.nvim_set_keymap(
+				mode,
+				"'",
+				"<cmd>lua require('mark-radar').scan(false)<cr>",
 				{ silent = true, noremap = true }
 			)
 		end
@@ -40,7 +46,7 @@ local function clean_up(top_line, bottom_line)
 	vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
 end
 
-local function scan()
+local function scan(jump_to_column)
 	local win_info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
 	local top_line, bottom_line = win_info.topline, win_info.botline
 	local bottom_line_column_count = string.len(vim.api.nvim_buf_get_lines(0, bottom_line - 1, bottom_line, false)[1])
@@ -72,7 +78,7 @@ local function scan()
 
 		if type(key) == "number" then
 			local key_str = vim.fn.nr2char(key)
-			motion.jump(mark_list, key_str)
+			motion.jump(mark_list, key_str, jump_to_column)
 			-- vim.api.nvim_feedkeys(key_str, "", true)
 			break
 		end
--- lua/mark-radar/motion.lua
+++ lua/mark-radar/motion.lua
@@ -1,8 +1,9 @@
-local function jump(mark_list, str)
+local function jump(mark_list, str, to_column)
 	for _, mark in ipairs(mark_list) do
 		if str == mark.mark:sub(2) then
 			local line, col = mark.pos[2], mark.pos[3] - 1
-            vim.api.nvim_win_set_cursor(0, { line, col })
+            local target_col = to_column and col or 0
+            vim.api.nvim_win_set_cursor(0, { line, target_col })
 		end
 	end
 end
